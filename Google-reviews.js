14a3f413412948ef88ea47226fd4bd64d5e5a566
globalstash15 <134397561+globalstash15@users.noreply.github.com>
2023-10-19 15:06:49 +0700

    Update Google-reviews.js

--- Google-reviews.js
+++ Google-reviews.js
@@ -1,11 +1,12 @@
+const fetch = require('node-fetch');
+
 // Replace 'YOUR_API_KEY' with your actual Google API key
 const apiKey = 'AIzaSyDGYIbOkIVI6AGATXxbNmSni15YOOlupFw';
 
-// Replace 'PLACE_ID_FIELD' with the Webflow field that holds the Place ID for each business
+// Replace 'PLACE_ID' with the Place ID for the business
 const urlParams = new URLSearchParams(window.location.search);
 const placeId = urlParams.get('placeId');
 
-// Create a function to fetch and display reviews
 async function fetchAndDisplayReviews() {
   const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`;
 
@@ -13,17 +14,12 @@ async function fetchAndDisplayReviews() {
     const response = await fetch(url);
     const data = await response.json();
     const reviews = data.result.reviews;
-    const reviewsContainer = document.getElementById('reviews-container');
 
     for (const review of reviews) {
-      const reviewElement = document.createElement('div');
-      reviewElement.innerHTML = `
-        <p>${review.text}</p>
-        <p>Rating: ${review.rating}</p>
-        <p>Author: ${review.author_name}</p>
-        <hr>
-      `;
-      reviewsContainer.appendChild(reviewElement);
+      console.log(review.text);
+      console.log('Rating:', review.rating);
+      console.log('Author:', review.author_name);
+      console.log('-------------------');
     }
   } catch (error) {
     console.error(error);
