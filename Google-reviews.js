bcd9f5937120517700f08dfaa67a7f824cf381b7
globalstash15 <134397561+globalstash15@users.noreply.github.com>
2023-10-19 14:34:17 +0700

    Update Google-reviews.js

--- Google-reviews.js
+++ Google-reviews.js
@@ -1,48 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>JS Bin</title>
-</head>
-<body>
-  <div id='reviews-container'></div>
-<script>
-  // Replace 'YOUR_API_KEY' with your actual Google API key
-  const apiKey = 'AIzaSyDGYIbOkIVI6AGATXxbNmSni15YOOlupFw';
+// Replace 'YOUR_API_KEY' with your actual Google API key
+const apiKey = 'AIzaSyDGYIbOkIVI6AGATXxbNmSni15YOOlupFw';
 
-  // Replace 'PLACE_ID_FIELD' with the Webflow field that holds the Place ID for each business
-  const urlParams = new URLSearchParams(window.location.search);
-  const placeId = urlParams.get('placeId');
+// Replace 'PLACE_ID_FIELD' with the Webflow field that holds the Place ID for each business
+const urlParams = new URLSearchParams(window.location.search);
+const placeId = urlParams.get('placeId');
 
-  // Create a function to fetch and display reviews
-  async function fetchAndDisplayReviews() {
-    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`;
+// Create a function to fetch and display reviews
+async function fetchAndDisplayReviews() {
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`;
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const reviews = data.result.reviews;
-      const reviewsContainer = document.getElementById('reviews-container');
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const reviews = data.result.reviews;
+    const reviewsContainer = document.getElementById('reviews-container');
 
-      for (const review of reviews) {
-        const reviewElement = document.createElement('div');
-        reviewElement.innerHTML = `
-          <p>${review.text}</p>
-          <p>Rating: ${review.rating}</p>
-          <p>Author: ${review.author_name}</p>
-          <hr>
-        `;
-        reviewsContainer.appendChild(reviewElement);
-      }
-    } catch (error) {
-      console.error(error);
+    for (const review of reviews) {
+      const reviewElement = document.createElement('div');
+      reviewElement.innerHTML = `
+        <p>${review.text}</p>
+        <p>Rating: ${review.rating}</p>
+        <p>Author: ${review.author_name}</p>
+        <hr>
+      `;
+      reviewsContainer.appendChild(reviewElement);
     }
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  // Call the function to fetch and display reviews
-  fetchAndDisplayReviews();
-</script>
-
-</body>
-</html>
+// Call the function to fetch and display reviews
+fetchAndDisplayReviews();
