f6208bf9da6c97a4389a0230e155894fbf2fb764
globalstash15 <134397561+globalstash15@users.noreply.github.com>
2023-10-23 15:09:13 +0700

    Update Google-reviews.js

--- Google-reviews.js
+++ Google-reviews.js
@@ -3,11 +3,9 @@ import fetch from 'node-fetch';
 // Replace 'YOUR_API_KEY' with your actual Google API key
 const apiKey = 'AIzaSyDGYIbOkIVI6AGATXxbNmSni15YOOlupFw';
 
-const fetch = require('node-fetch');
-// Note: There's no direct equivalent for window.location.search in Node.js
-// You may need to pass the URL as a parameter or retrieve it differently.
-// ...
-const placeId = urlParams.get('placeId');
+// Get placeId from the command line arguments in a Node.js environment
+const args = process.argv.slice(2); // Remove the first two arguments (node and script filename)
+const placeId = args[0]; // Assuming placeId is the first argument
 
 async function fetchAndDisplayReviews() {
   const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=reviews&key=${apiKey}`;
