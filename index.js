d5d6611707e6ebb2abaac0b25ed0fd8304fd687f
Tobias Cichon <cichon@hbju.de>
2023-11-01 13:50:18 +0700

    More shizzzzzlllleeeee

--- index.js
+++ index.js
@@ -104,6 +104,25 @@ app.get('/', async (req, res) => {
                             font-size: 30px
                         }
 
+                        .ratings {
+                            position: relative;
+                            vertical-align: middle;
+                            display: inline-block;
+                            color: #b1b1b1;
+                            overflow: hidden;
+                        }
+                        .full-stars {
+                            position: absolute;
+                            left: 0;
+                            top: 0;
+                            white-space: nowrap;
+                            overflow: hidden;
+                            color: #fde16d;
+                        }
+                        .empty-stars:before, .full-stars:before {
+                            content:"\\2605\\2605\\2605\\2605\\2605";
+                            font-size: 14pt;
+                        }
                     </style>
                 </head>
                 <body>
@@ -142,6 +161,10 @@ app.get('/', async (req, res) => {
                                 <h2>
                                     <span class="large">${ getStars(data.result.rating)}</span>
                                 </h2>
+                                <div class="ratings">
+                                    <div class="empty-stars"></div>
+                                    <div class="full-stars" style="width:${ Number(data.result.rating) * 2 * 10 }%"></div>
+                                </div>
                             </div>
                         </div>
                     </section>
